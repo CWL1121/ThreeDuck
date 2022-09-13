6b27e0c96d846e1c432c40eefb5f11a160e05276
CWL1121 <allen107435265@gmail.com>
2022-09-14 04:10:54 +0800

    0914 ThreeDuck MultiPlayer

--- src/utils/test.js
+++ src/utils/test.js
@@ -8,6 +8,7 @@ import { threeToCannon, ShapeType } from 'three-to-cannon';
 import { Vec3 } from 'cannon-es';
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 import { WireframeGeometry } from 'three';
+import {io} from 'socket.io-client'
 
 export default class Three{
     constructor(){
@@ -15,6 +16,9 @@ export default class Three{
     }
 
     init(){
+        this.playerInfo = [];
+        this.socket = io('http://localhost:3000');
+        console.log(this.socket.id)
         this.scene = new THREE.Scene();
         this.camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 
@@ -23,7 +27,6 @@ export default class Three{
         document.body.appendChild( this.renderer.domElement );
 
         this.controls = new OrbitControls( this.camera, this.renderer.domElement );
-        //controls.update() must be called after any manual changes to the camera's transform
         this.camera.position.set( 0, 20, 40 );
         this.controls.update();
 
@@ -43,9 +46,9 @@ export default class Three{
         .load( [ 'px.jpg', 'nx.jpg', 'py.jpg', 'ny.jpg', 'pz.jpg', 'nz.jpg' ] );
 
         this.createBoxes();
-        console.log("a")
-
+        this.updatePlace()
         this.animate();
+    
     }
 
     loadModels(){
@@ -92,13 +95,67 @@ export default class Three{
         this.boxMesh.castShadow = true
         this.boxMesh.receiveShadow = true
 
+    }
+
+    addSquareBox(info){
+        try {
+            this.check(info)
+            this.geometry = new THREE.BoxGeometry( 10, 10, 10 );
+            this.material = new THREE.MeshBasicMaterial( {color:0x0044BB} );
+            this.cube = new THREE.Mesh( this.geometry, this.material );
+            this.cube.position.x = info.pos.position.x;
+            this.cube.position.y = info.pos.position.y;
+            this.cube.position.z = info.pos.position.z;
+            this.cube.name = info.id
+            this.scene.add( this.cube );
+        } catch (error) {
+            
         }
 
+    }
+
     animate() {
+        this.socket.emit('move',{
+            position:this.controls.object.position
+        })
         this.renderer.setAnimationLoop( this.animate.bind((this)) );
         this.renderer.render( this.scene, this.camera );
         this.controls.update();
-        console.log(this.scene)
+        // this.check()
+        this.playerInfo.forEach(box=>{
+            this.addSquareBox(box)
+        })
+        // console.log(this.scene.children)
     }
-
+    updatePlace(){
+        this.socket.on('replace',(info)=>{
+            this.playerInfo = info.players;
+        })
+    }
+    check(info){
+        let ans = true
+        this.scene.children.forEach(child=>{
+            if (child.name) {
+                if(child.name == info.id){
+                    this.ans = false;
+                    this.updatePosition(child.name,info.pos.position)
+                    throw {}
+                }else{
+                    this.ans = true
+                }
+            }
+        })
+        return ans;
+    }
+    updatePosition(info,pos){
+        this.scene.children.forEach(child=>{
+            if (child.name) {
+                if(child.name == info){
+                    child.position.x = pos.x;
+                    child.position.y = pos.y;
+                    child.position.z = pos.z;
+                }
+            }
+        })
+    }
 }
--- src/views/test.vue
+++ src/views/test.vue
@@ -17,7 +17,27 @@
         animate();
     })
 
+    function Element( id, x, y, z, ry ) {
 
+        const div = document.createElement( 'div' );
+        div.style.width = '480px';
+        div.style.height = '360px';
+        div.style.backgroundColor = '#000';
+
+        const iframe = document.createElement( 'iframe' );
+        iframe.style.width = '480px';
+        iframe.style.height = '360px';
+        iframe.style.border = '0px';
+        iframe.src = [ 'https://www.youtube.com/embed/', id, '?rel=0' ].join( '' );
+        div.appendChild( iframe );
+
+        const object = new CSS3DObject( div );
+        object.position.set( x, y, z );
+        object.rotation.y = ry;
+
+        return object;
+
+    }
 
     init();
     animate();
